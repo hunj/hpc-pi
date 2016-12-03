eb2f643aa9c22b59a8dd9d2738d935561be503a8
Hun Jae Lee <hunjael@gmail.com>
2016-12-02 19:27:06 -0500

    use Decimal(float(math.pi))

--- time/pi_monte-carlo.py
+++ time/pi_monte-carlo.py
@@ -6,7 +6,6 @@ import datetime, time
 import sys
 import math
 
-
 # checks whether given x, y coordinate is inside unit circle boundary.
 # input:
 #   - x: the x-coordinate of the point
@@ -22,12 +21,15 @@ def main():
 
     # set up and start timer
     endTime = datetime.datetime.now() + datetime.timedelta(minutes=int(sys.argv[1]))
+    getcontext().prec = 10
 
     # the sweet part. constantly approximate Pi value.
     while True:
         # breaks when time's up
         if datetime.datetime.now() >= endTime:
             break
+
+        getcontext().prec += 5
 
         # monte-carlo method
         iterations += 1
@@ -37,11 +39,11 @@ def main():
 
     # multiply 4, since we are only on the first quadrant of the unit circle.
     calculated_pi = Decimal(4) * Decimal(num_hit) / Decimal(iterations)
-
+    pi_decimal = Decimal(float(math.pi))
     # display cute message and quit.
     print "Calculated pi: ", calculated_pi
-    print "Difference to exact value of pi: ", calculated_pi - math.pi
-    print "Error: (approx-exact)/exact = ", (calculated_pi - math.pi) / math.pi * 100, "%"
+    print "Difference to exact value of pi: ", calculated_pi - pi_decimal
+    print "Error: (approx-exact)/exact = ", (calculated_pi - pi_decimal) / pi_decimal * 100, "%"
 
 if __name__ == '__main__':
     main()
